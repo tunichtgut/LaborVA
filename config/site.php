e686fd6e7ed42dc187c951b006eddd38fce1e699
Matthis Radke <matthis@do-smile.de>
2013-02-04 16:22:13 +0100

    Sass migration angefangen. Muss noch die Pfade für die Hauptcssdatei neu setzen!

--- config/site.php
+++ config/site.php
@@ -1,6 +1,6 @@
-<?php 
-define('DB_SERVER', 'mysql5.do-smile.de');
-define('DB_USERNAME', 'db326221_3');
-define('DB_PASSWORD', '.KOI9hm_zGe[');
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'ohms');
+define('DB_PASSWORD', 'A246_s832');
 define('DB_DATABASE', 'db326221_3');
 define('PASSWORD_SALT', '6LtjCJqFoFpYckV6LTTNRI3U3hrb7wBETO00HwUlqgwbZKMWPMUah5NPf1kt89mX');?><?php define('DIRNAME_APP_UPDATED', 'concrete5.6.0.2');?>
--- themes/ohms/default.php
+++ themes/ohms/default.php
@@ -1,23 +1,23 @@
-<?php 
+<?php
 defined('C5_EXECUTE') or die("Access Denied.");
 $this->inc('elements/header.php'); ?>
 
 	<div id="central">
 		<div id="sidebar">
-			<?php 
+			<?php
 			$as = new Area('Sidebar');
 			$as->display($c);
-			?>		
+			?>
 		</div>
-		
-		<div id="body">	
-			<?php 
+
+		<div id="body">
+			<?php
 			$a = new Area('Main');
 			$a->display($c);
 			?>
 		</div>
-		
-		<div class="spacer">&nbsp;</div>		
+
+		<div class="spacer">&nbsp;</div>
 	</div>
 
 <?php  $this->inc('elements/footer.php'); ?>
